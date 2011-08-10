58ac490fa1a5389377b8db04557ed1420579fda5
Stanley Biggs <stanley@gofuseit.com>
2011-08-10 20:39:56 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Scbfirst::Application.config.secret_token = 'd7d85d17278fbefecad6c617b42a4637cf30bff5787c2674d0bad9246e5159d9c6a4857985796a83556ed3b183add5f156b1d5e47efa1fa16159f7e14432537a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Scbfirst::Application.config.session_store :cookie_store, :key => '_scbfirst_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Scbfirst::Application.config.session_store :active_record_store
